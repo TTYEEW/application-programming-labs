c436113aae8391775e598e394b33a14464e673f6
ttyeew <ttyeew@gmail.com>
2024-11-06 03:23:53 +0400

    Some fixes have been added

--- main.py
+++ main.py
@@ -3,31 +3,54 @@ import re
 from collections import Counter
 
 ##
-def parse():
+def parse() -> str:
+    """
+    Парсинг аргументов командной строки
+    :return: путь к файлу
+
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('file_path', type=str, help='data.txt')
     return parser.parse_args().file_path
 
-def file_read(path_to_file):
+def file_read(path_to_file: str) -> str:
+    """
+    Прочесть файл и вернуть его содержимое
+    :param path-to_file: путь к файлу с анкетами
+    :return: содержимое файла(текст)
+
+    """
     try:
         with open(path_to_file, 'r', encoding='utf-8') as file:
-            data = file.read()
-        return data
+            return file.read()
     except FileNotFoundError:
         raise FileNotFoundError(f"File {path_to_file} not found.")
     except Exception as e:
         raise Exception(f"Error reading file {e}")
 
-def main():
-    data = file_read(parse())
+def find_phone_pattern(data: str) -> tuple[str, int]:
+    """
+    Найти наиболее часто встречающийся код оператора и вернуть его и количество раз, которое он встречается
+    :param data: текст из файла с анкетами
+    :return: наиболее часто встречающийся код оператора 
+
+    """
     phone_pattern = re.findall(r'\b\d{3}\b', data)
     number_of_codes = Counter(phone_pattern)
-    common_code = number_of_codes.most_common(1)
+    if number_of_codes :
+        return number_of_codes.most_common(1)
+    else: None
 
-    if common_code:
-        print(f"The most common code: {common_code[0][0]} is encountered {common_code[0][1]} times")
-    else:
-        print("Phone numbers not found")
+def main() -> None:
+    try:
+        data = file_read(parse())
+        common_code = find_phone_pattern(data)
+        if common_code:
+            print(f"The most common code: {common_code[0][0]} is encountered {common_code[0][1]} times")
+        else:
+            print("Phone numbers not found")
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
