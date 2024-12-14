2516310cf267f3744051c3baac46d9b9119cc020
ttyeew <ttyeew@gmail.com>
2024-12-14 17:28:46 +0400

    add rows for height width

--- Lab4/data_frame.py
+++ Lab4/data_frame.py
@@ -1,3 +1,4 @@
+import cv2
 import pandas as pd
 
 def load_annotation(annotation_file: str) -> pd.DataFrame:
@@ -8,3 +9,25 @@ def load_annotation(annotation_file: str) -> pd.DataFrame:
     
     """
     return pd.read_csv(annotation_file, names=["absolute_path", "relative_path"], header=0)
+
+def add_rows_for_image(df: pd.DataFrame) -> None:
+    """
+    Добавляет столбцы высоты, ширины, глубины и площади изображений
+    
+    """
+    heights, widths, depths, areas = [], [], [], []
+    for _, row in df.iterrows():
+        image = cv2.imread(row["absolute_path"])
+        if image is not None:
+            h, w, d = image.shape
+            heights.append(h)
+            widths.append(w)
+            depths.append(d)
+            areas.append(h * w)
+        else:
+            raise FileNotFoundError(f"image {row["absolute_path"]} not found")
+
+    df["height"] = heights
+    df["width"] = widths
+    df["depth"] = depths
+    df["area"] = areas
--- Lab4/main.py
+++ Lab4/main.py
@@ -1,13 +1,14 @@
 from parser import parse
-from data_frame import (load_annotation)
+from data_frame import (load_annotation, 
+                        add_rows_for_image)
 
 ##Add files via upload
 def main() -> None:
     try:
         arg = parse()
         df = load_annotation(arg.annotation_file)
+        add_rows_for_image(df)
         print(df)
-
     except Exception as e:
         print(e)
 
